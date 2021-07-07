715ad9a80d6f47250f69dcb93a3737e59ff70553
= <=>
2021-07-07 17:34:03 +0700

    a

--- first_test/urls.py
+++ first_test/urls.py
@@ -15,6 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.conf.urls import url
+from django.conf.urls.static import static
+
+from django.conf import settings
 from . import views
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
